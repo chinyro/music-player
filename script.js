242310ec6cbe19e17d3840a2cba42d57d0bf8a1f
sydrob <55166840+sydrobhez@users.noreply.github.com>
2023-03-10 23:21:00 -0600

    updated progress bar function

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const progressContainer = document.getElementById('progress-container');
 const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -92,10 +94,12 @@ loadSong(songs[songIndex]);
 function updateProgressBar(e) {
     if(isPlaying) {
         const { duration, currentTime } = e.srcElement;
-        console.log(duration, currentTime);
         // Update progress bar width
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
+        // Calculate the display for the duration
+        const durationMinutes = Math.floor(duration / 60);
+        console.log('minutes', durationMinutes);
     }
 }
 
