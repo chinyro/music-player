b835ac3c3dc6e41d935526748a7a61d158988567
sydrob <55166840+sydrobhez@users.noreply.github.com>
2023-03-11 14:27:26 -0600

    updated duration and current time

--- script.js
+++ script.js
@@ -100,6 +100,24 @@ function updateProgressBar(e) {
         // Calculate the display for the duration
         const durationMinutes = Math.floor(duration / 60);
         console.log('minutes', durationMinutes);
+        let durationSeconds = Math.floor(duration % 60);
+        if(durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        console.log('seconds', durationSeconds);
+        // Delay switching duration Element to avoid NaN
+        if(durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // Calculate the display for the current
+        const currentMinutes = Math.floor(currentTime / 60);
+        console.log('minutes', currentMinutes);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if(currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        console.log('seconds', currentSeconds);
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
 }
 
